52dd8512ea9b7c357df4624db7cb018a87566acc
NARATSAPORN HEAMRA <naratsaporn@gmail.com>
2020-11-09 11:40:45 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React ,{ useState} from 'react';
+import {BrowserRouter , Route , Link} from 'react-router-dom';
+
 import './App.css';
 
-import Navbar from './Navbar';
-import Input from './Input';
-import Post from './Post';
+import Navbar from './temples/Navbar';
+import Input from './temples/Input';
+import Post from './temples/Post';
+
+
+
 let id = 1;
+const Home = () => <div>Home Page</div>
+const About = () => <div>About Page</div>
+
 function App() {
   const [posts , setposts] = useState([]);
 
@@ -20,12 +28,26 @@ function App() {
   }
 
   return (
+    
     <div className="App">
       <Navbar/>
-      <Input addPost={addPost}/>
-      {posts.map((post)=>
-       <Post key={post.id} id={post.id} title={post.title}  deletPost={deletPost}/>
-       )}
+      <BrowserRouter>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <div style={{ width: "60%" , margin : "50px auto" }}>
+        <Route exact path="/">
+            <Home />
+            <Input addPost={addPost}/>
+            {posts.map((post)=>
+            <Post key={post.id} id={post.id} title={post.title}  deletPost={deletPost}/>
+            )}
+        </Route>
+        <Route exact path="/about">
+            <About />
+        </Route>
+      </div>
+      </BrowserRouter>
+  
 
     </div>
   );
